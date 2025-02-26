387ed46812d82b18748bb990750b68b6018ce0eb
Nikhil Dasar <kakashihatakesh6@gmail.com>
2025-02-26 18:20:58 +0530

    feat: added vercel.json

--- index.ts
+++ index.ts
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000", // Allow requests from frontend
+    origin: `${process.env.HOST}`, // Allow requests from frontend
     // origin: "*", // Allow requests from frontend
     methods: "GET,POST,PUT,DELETE", // Allowed HTTP methods
     allowedHeaders: "Content-Type,Authorization", // Allowed headers
@@ -89,7 +89,7 @@ app.delete("/api/tasks/:id", async (req: Request<{ id: string }>, res: any) => {
 // Vercel compatibility
 export default app;
 
-if (process.env.NODE_ENV !== "vercel") {
+if (process.env.NODE_ENV !== "production") {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
